ba0b69ef187ddd7da1e10e8d6d13331db343e295
davdroman <d@vidroman.dev>
2021-09-28 16:01:17 +0100

    Expand compatible platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "MultiModal",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
     ],
     products: [
         .library(
