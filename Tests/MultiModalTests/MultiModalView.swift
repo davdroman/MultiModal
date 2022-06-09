cd6a05ffc52c7143fe9d3124a7ec5d5b9a88d6cb
David Roman <2538074+davdroman@users.noreply.github.com>
2022-06-09 01:03:38 +0100

    Use `buildPartialBlock` for Swift 5.7+

--- Tests/MultiModalTests/MultiModalView.swift
+++ Tests/MultiModalTests/MultiModalView.swift
@@ -3,24 +3,42 @@ import SwiftUI
 
 // This declaration is used to prove the `multiModal`
 // modifier compiles successfully on all Swift versions.
+// Additionally, it ensures that unlimited modals can be
+// used from Swift 5.7 onwards thanks to `buildPartialBlock`.
 
 // With a bit of time and effort, it could be expanded into a UI test suite.
 
 struct MultiModalView: View {
-    @State var sheetAPresented = false
-    @State var sheetBPresented = false
-    @State var sheetCPresented = false
-
     var body: some View {
-        VStack(spacing: 20) {
-            Button("Sheet A") { self.sheetAPresented = true }
-            Button("Sheet B") { self.sheetBPresented = true }
-            Button("Sheet C") { self.sheetCPresented = true }
-        }
-        .multiModal {
-            $0.sheet(isPresented: $sheetAPresented) { Text("Sheet A") } // works
-            $0.sheet(isPresented: $sheetBPresented) { Text("Sheet B") } // works
-            $0.sheet(isPresented: $sheetCPresented) { Text("Sheet C") } // works
+        EmptyView().multiModal {
+            #if compiler(>=5.7)
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 1") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 2") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 3") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 4") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 5") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 6") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 7") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 8") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 9") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 10") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 11") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 12") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 13") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 14") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 15") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 16") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 17") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 18") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 19") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 20") }
+            #else
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 1") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 2") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 3") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 4") }
+            $0.sheet(isPresented: .constant(false)) { Text("Sheet 5") }
+            #endif
         }
     }
 }
